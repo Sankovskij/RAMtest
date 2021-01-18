7daca4e133c28dab07ba215a5e7afd615f4ab6c3
Sankovskij <Sanknet@yandex.ru>
2021-01-18 17:23:29 +0300

    ready to changes

--- app/src/main/java/sankovskij/test/ramtest/onboarding/model/OnBoadrdingData.kt
+++
@@ -1,9 +0,0 @@
-package sankovskij.test.ramtest.onboarding.model
-
-import android.graphics.drawable.Drawable
-
-class OnBoadrdingData (
-    var onboadringHeader : String,
-    var onboardingText : String,
-    var onboardingImg : Int
-)
