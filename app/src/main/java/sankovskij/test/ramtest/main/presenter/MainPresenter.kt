a264254e598d70f4c0c24d3923de59fcdf85015b
Sankovskij <Sanknet@yandex.ru>
2021-01-18 17:27:00 +0300

    убираю лишнее

--- app/src/main/java/sankovskij/test/ramtest/main/presenter/MainPresenter.kt
+++ app/src/main/java/sankovskij/test/ramtest/main/presenter/MainPresenter.kt
@@ -7,14 +7,10 @@ import sankovskij.test.ramtest.main.MainView
 
 class MainPresenter(): MvpPresenter<MainView>() {
 
-    val mi = ActivityManager.MemoryInfo()
-
-    fun memoryPercentAvail() = mi.availMem / mi.totalMem as Double * 100.0
-
-    fun memoryAvailableMegs() = (mi.availMem / 0x100000L).toDouble()
+   
 
         //буду менять
-    
+
 
 
 }
